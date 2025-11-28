1f7fdc108793777b93eab6fb1f261f66ae9cdce2
sickee2 <sickee2@hotmail.com>
2025-11-28 18:23:51 +0800

    git setting

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,6 @@ int main() {
   using namespace gr;
   console::writeln("helle world {}", gr::toy::chrono::now());
   console::writeln("test .gitignore");
+  console::writeln("new modify for git testing");
   return 0;
 }
